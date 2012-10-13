47ef86a5d85b18d3e8309da320b8477f34023f95
Patrick Ryan <phiat99@gmail.com>
2012-10-13 04:47:24 -0300

    Update src/derri/core.clj
    added game of life

--- src/derri/core.clj
+++ src/derri/core.clj
@@ -25,21 +25,30 @@
 
                       
 
-      
-(def  t1 [      "      " 
-                " ##   "
-                " ##   "
-                "   ## "
-                "   ## "
-                "      "])
-(def t2 [       "      " 
-                " ##   "
-                " #    "
-                "    # "
-                "   ## "
-                "      "])
+;; Game of Life
+(fn [xcoll]
+  ;; reconstructs flattened vec (\space \space \space \space \# \# ...) --> ("    ##" ...)
+  (map #(reduce str %) (partition (count (first xcoll))
 
-(defn test-life [a b] ())
-
-    
-
+   (map #(let [cell (nth (reduce str xcoll) %)  ;; cell = current element
+              n ((fn [coll i]                   ;; 'get-neighbors' inline function
+    (let [w (count (first coll))                ;; w = width
+         h (count coll)                         ;; h = height
+         x (mod i w)                            ;; x y coords
+         y (int (/ i w))
+         life \#]                               ;; life
+   ;; sum up neighbors
+     (reduce + (list (if (> x 0) (if (= (nth (nth coll y) (dec x)) life) 1 0) 0)                     
+                     (if (> y 0) (if (= (nth (nth coll (dec y)) x) life) 1 0) 0)
+                     (if (< x (dec w)) (if (= (nth (nth coll y) (inc x)) life) 1 0) 0) 
+                     (if (< y (dec h)) (if (= (nth (nth coll (inc y)) x) life) 1 0) 0)
+                     (if (and (> x 0) (> y 0))       (if (= (nth (nth coll (dec y)) (dec x)) life) 1 0) 0)
+                     (if (and (> x 0) (< y (dec h))) (if (= (nth (nth coll (inc y)) (dec x)) life) 1 0) 0)
+                     (if (and (< x (dec w)) (> y 0)) (if (= (nth (nth coll (dec y)) (inc x)) life) 1 0) 0)
+                     (if (and (< x (dec w)) (< y (dec h))) (if (= (nth (nth coll (inc y)) (inc x)) life) 1 0) 0))))) xcoll %)]           (if (= cell \#)
+            (case n            ;; evaluate living cells
+              (0 1) " "        ;;  0 or 1 --> dead
+              (2 3) \#         ;;  2 or 3 --> alive
+              " ")             ;;  > 3    --> dead
+            (if (= n 3) \# " ")))  ;; dead cells with 3 neighbors come alive
+       (range 0 (* (count (first xcoll)) (count xcoll)))))))  ; range to map over indexes
